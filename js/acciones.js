2ad14c39aa804b44ae955adfbc23f7d461c96fd0
AL25J11 <zairaMartinez50@hotmail.com>
2013-06-13 07:53:08 -0500

    ESTE PROGAMA ME CONECTA CON MI EQUIPO DE COMPUTO  VERCION 1.1

---
+++ js/acciones.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function LoginConectar(U,P)
+{
+	datos="usuario="+U+"& Password="+P;
+	$.ajax({type:POST,url:"192.168.1.175/Sitio6/log_in_m.php",data:datos}).done(function(msg){
+	alert(msg);
+	if (msg==""||msg==null)
+	    {
+		alert("usuario incorecto");
+		}
+	else 
+    	{
+		alert("bienvenido");
+		}
+	
+});//fuction
+}
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function (){ 
+	$('.Enviar').tap (function (){
+		var formulario=$(this).parents('form');
+		var usuario= document.getElementByID('Usuario').value;
+		var password= document.getElementByID('Password').value;
+		LoginConectar(usuario,password);
+	});//tap
+	});//deviceready
+});    //ready
+
+
